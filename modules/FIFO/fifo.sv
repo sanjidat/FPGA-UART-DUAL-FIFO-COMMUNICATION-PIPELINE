9b5bbe2b20f18eee6f8a6dbf9fdc7494da3e09f4
sanjidat <sanjidaorin11@gmail.com>
2026-07-21 13:53:15 +0200

    Synchronous FIFO Module added

---
+++ modules/FIFO/fifo.sv
@@ -0,0 +1,55 @@
+`timescale 1ns/1ps
+
+module fifo(
+	input logic clk,
+	input logic rst,
+	
+	input logic rd_en,
+	input logic wr_en,
+	
+	input logic  [7:0] data_in,
+	output logic [7:0] data_out,
+	
+	output logic full, 
+	output logic empty
+
+);
+
+logic [7:0] memory [0:7];
+logic [2:0] wr_ptr;
+logic [2:0] rd_ptr;
+logic [3:0] count;
+
+integer i;
+
+always_ff @(posedge clk) begin
+	if (rst) begin
+		wr_ptr   <= 3'd0;
+		rd_ptr   <= 3'd0;
+		count    <= 4'd0;
+		data_out <= 8'd0;	
+		
+		for (i= 0; i<8 ; i++)
+			memory[i] <= 8'd0;
+	end
+	
+	else begin
+		if (wr_en && count<4'd8) begin
+			memory[wr_ptr] <= data_in;
+			wr_ptr <= wr_ptr + 1;
+			count <= count +1;
+		end 
+	
+		if (rd_en && count>4'd0) begin
+			data_out <= memory[rd_ptr];
+			rd_ptr <= rd_ptr + 1;
+			count <= count -1;
+		end
+	
+	end	
+end
+
+assign full  = (count == 4'd8);
+assign empty = (count == 4'd0);
+
+endmodule
